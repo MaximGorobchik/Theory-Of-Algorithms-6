49046c3ebf3f13d30228a7cacc51ccb3a51d9457
МАКСИМ <@FSB>
2023-01-04 22:09:48 +0200

    Добавьте файлы проекта.

---
+++ Теорія Алгоритмів лаба 6/Program.cs
@@ -0,0 +1,22 @@
+﻿void Function(double t, double n)
+{
+    var T = 4 * t * (n / 5) + 2 * n;
+    Console.WriteLine($"T(n) = {T}");
+}
+var input = Console.ReadLine();
+while (!input.Equals("end"))
+{
+    if (input.Equals("start"))
+    {
+        Console.Write("Введiть T: "); var x = double.Parse(Console.ReadLine());
+        Console.Write("Введiть n: "); var y = double.Parse(Console.ReadLine());
+        Console.ForegroundColor = ConsoleColor.Red;
+        Function(x, y);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    else
+    {
+        return;
+    }
+    input = Console.ReadLine();
+}
